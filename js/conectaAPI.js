f32dc6ba22be9d74cb2d6f99acad32054e0ed156
Thiago <thiagocordeirodantas0@Gmail.com>
2023-02-15 00:33:22 -0300

    criando requisicao post

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -2,10 +2,31 @@ async function listaVideos (){
     const conexao = await fetch('http://localhost:3000/videos')
     const conexaoConvertida = await conexao.json();
   
+    return conexaoConvertida;   
+}
+
+async function criaVideo(){
+    const conexao = await fetch("http://localhost:3000", {
+       method: "POST",
+       headers: {
+        "Content-type": "application/json"
+       },
+       body: JSON.stringify({
+        titulo: titulo,
+        descricao: `${descricao} mil visualizações`,
+        url: url,
+        imagem: imagem
+       })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
     return conexaoConvertida;
+
 
 }
 
 export const conectaAPI = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
